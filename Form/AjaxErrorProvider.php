af1a1f2d3861aed430c20ab20164a6a168a8715d
David Tee <davidkmo@gmail.com>
2011-05-01 23:55:45 -0700

    Added AjaxErrorProvider as part of AssetBundle.

---
+++ Form/AjaxErrorProvider.php
@@ -0,0 +1,48 @@
+<?php
+namespace Odl\AssetBundle\Form;
+
+use Symfony\Component\Form\Form;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class AjaxErrorProvider
+{
+	private $translator;
+
+	public function __construct(TranslatorInterface $translator)
+	{
+		$this->translator = $translator;
+	}
+
+	public function getErrors(Form $form)
+	{
+        $errors = array();
+
+        $formView = $form->createView();
+        $key = $formView->get('id');
+
+		if (!$form->isValid())
+		{
+			// User translator to translate
+			$errorObjects = $form->getErrors();
+			foreach ($errorObjects as $errorObject)
+			{
+				$errors[$key][] = $this->translator->trans(
+	                $errorObject->getMessageTemplate(),
+	                $errorObject->getMessageParameters(),
+	                'validators'
+	            );
+			}
+		}
+		else
+		{
+			$errors[$key] = null;
+		}
+
+		foreach ($form->getChildren() as $childName => $form)
+		{
+			$errors = array_merge($errors, $this->getErrors($form));
+		}
+
+		return $errors;
+	}
+}
