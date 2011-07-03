450d0d487025c7690c485f870831987d051a7f6f
David Tee <davidkmo@gmail.com>
2011-07-03 02:49:00 -0700

    First crack at creating forms with ajax validation easier.

---
+++ Form/FormHandler.php
@@ -0,0 +1,59 @@
+<?php
+namespace Odl\AssetBundle\Form;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\FormFactory;
+
+abstract class FormHandler
+{
+    protected $formFactory;
+    protected $request;
+    protected $ajaxErrorProvider;
+
+    protected $errors;
+    protected $form;
+
+    public function __construt(FormFactory $formFactory, Request $request, AjaxErrorProvider $ajaxErrorProvider)
+    {
+        $this->formFactory = $formFactory;
+        $this->request = $request;
+        $this->ajaxErrorProvider = $ajaxErrorProvider;
+
+        $this->createForm();
+        $this->process();
+    }
+
+    abstract protected function createForm()
+    {
+
+    }
+
+    abstract protected function onSuccess($values) {
+
+    }
+
+    public function process()
+    {
+        if ($this->request->getMethod() == 'POST')
+        {
+            $this->form->bindRequest($this->request);
+            if ($this->form->isValid()) {
+
+            }
+            else {
+                $this->errors = $this->ajaxErrorProvider->getErrors($this->form);
+            }
+        }
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    public function getForm()
+    {
+        return $this->form;
+    }
+}
