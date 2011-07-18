38939571e5cdff5216ad9062d596b3f7be959339
David Tee <davidkmo@gmail.com>
2011-07-17 23:37:24 -0700

    Update ajax form, added template file for symfony to handle errors.

--- Form/AjaxErrorProvider.php
+++ Form/AjaxErrorProvider.php
@@ -6,41 +6,42 @@ use Symfony\Component\Translation\TranslatorInterface;
 
 class AjaxErrorProvider
 {
-	private $translator;
+    private $translator;
 
-	public function __construct(TranslatorInterface $translator)
-	{
-		$this->translator = $translator;
-	}
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
 
-	public function getErrors(Form $form, &$errors = array())
-	{
+    public function getErrors(Form $form, &$errors = array())
+    {
         $formView = $form->createView();
         $key = $formView->get('id');
 
-		if (!$form->isValid())
-		{
-			// User translator to translate
-			$errorObjects = $form->getErrors();
-			foreach ($errorObjects as $errorObject)
-			{
-				$errors[$key][] = $this->translator->trans(
-	                $errorObject->getMessageTemplate(),
-	                $errorObject->getMessageParameters(),
-	                'validators'
-	            );
-			}
-		}
-		else
-		{
-			$errors[$key] = null;
+        if (!$form->isValid())
+        {
+            // User translator to translate
+            $errorObjects = $form->getErrors();
+            $formTypes = $formView->get('types');
+            if (in_array('repeated', $formTypes)) {
+                //$key .= '_' . $formView->get('name');
+            }
+
+            foreach ( $errorObjects as $errorObject )
+            {
+                $errors[$key][] = $this->translator->trans($errorObject->getMessageTemplate(), $errorObject->getMessageParameters(), 'validators');
+            }
+        }
+        else
+        {
+            $errors[$key] = null;
+        }
+
+        foreach ( $form->getChildren() as $childName => $form )
+        {
+            $this->getErrors($form, $errors);
 		}
 
-		foreach ($form->getChildren() as $childName => $form)
-		{
-			$this->getErrors($form, $errors);
-		}
-		
 		return $errors;
 	}
 }
